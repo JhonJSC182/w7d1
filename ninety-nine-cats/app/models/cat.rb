c8cdaa8d0290cd9af145638918394710455f066f
Albert Dedushi <adedushi@hotmail.com>
2023-11-27 14:56:33 -0500

    add validations

---
+++ ninety-nine-cats/app/models/cat.rb
@@ -0,0 +1,26 @@
+require "action_view"
+
+class Cat < ApplicationRecord
+    CAT_COLORS = ['orange', 'black', 'white', 'calico' ]
+
+    include ActionView::Helpers::DateHelper
+    validates :birth_date, presence: true
+    validates :color, presence: true, inclusion: { in: CAT_COLORS }
+    validates :name, presence: true
+    validates :sex, presence: true, inclusion: { in: ['M', 'F'] }
+    validate :birth_date_cannot_be_future
+
+    def age
+        ((Time.zone.now - birth_date.to_time) / 1.year.seconds).floor
+    end
+
+    private
+    def birth_date_cannot_be_future
+        if birth_date > Date.today
+            errors.add(:birth_date, "cannot be after today\'s date")
+        end
+    end
+
+
+
+end
